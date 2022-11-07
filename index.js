d4adac81bb8aec4c4cb23316f77d3614e23f6007
Prince Chaudhary <princerathi1989@gmail.com>
2022-11-07 21:12:55 +0530

    Cleanup

--- index.js
+++ index.js
@@ -1,104 +1,53 @@
-function toUpperCase(data) {
-  const casing = new UpperCase(data);
-  return casing.convert();
-}
-
-function toLowerCase(data) {
-  const casing = new LowerCase(data);
-  return casing.convert();
-}
-
-function toCamelCase(data) {
-  const casing = new CamelCase(data);
-  return casing.convert();
-}
-
+function toCamelCase(data) {return (new CamelCase(data)).convert(); }
+function toLowerCase(data) {return (new LowerCase(data)).convert(); }
+function toUpperCase(data) {return (new UpperCase(data)).convert(); }
 class Json {
   data;
-
-  constructor(data) {
-    this.data = data;
-  }
-
-  getData() {
-    return this.data;
-  }
-
+  constructor(data) { this.data = data; }
+  getData() { return this.data; }
   convert() {
     if (Array.isArray(this.getData())) {
       return this.convertArray(this.getData());
     }
     return this.changeCase(data);
   }
-
   convertArray(data) {
     const result = data.reduce((next, item) => {
       return [...next, this.changeCase(item)];
     }, []);
     return result;
   }
-
   changeCase(data) {
     const result = Object.entries(data).reduce((next, [key, value]) => {
       let nestedObject = this.getConvertedNestedObject(value);
       const propertyName = this.getPropertyName(key);
-      return {
-        ...next,
-        [propertyName]: nestedObject || value
-      };
+      return { ...next, [propertyName]: nestedObject || value };
     }, {});
     return result;
   }
-
   getConvertedNestedObject(value) {
-    if (typeof value === 'object') {
-      return this.changeCase(value);
-    }
-    if (Array.isArray(value)) {
-      return this.convertArray(value);
-    }
+    if (typeof value === 'object') { return this.changeCase(value); }
+    if (Array.isArray(value)) { return this.convertArray(value); }
     return null;
   }
-
-  getPropertyName(key) {
-    return key;
-  }
+  getPropertyName(key) { return key; }
 }
-
 class CamelCase extends Json {
-  constructor(data) {
-    super(data);
-  }
-
+  constructor(data) { super(data); }
   getPropertyName(key) {
     let propertyName = key.toLowerCase();
-    if (key.length > 2) {
-      propertyName = key[0].toLowerCase() + key.slice(1);
-    }
+    if (key.length > 2) { propertyName = key[0].toLowerCase() + key.slice(1); }
     return propertyName;
   }
 }
-
 class LowerCase extends Json {
-  constructor(data) {
-    super(data);
-  }
-
-  getPropertyName(key) {
-    return key.toLowerCase();
-  }
+  constructor(data) { super(data); }
+  getPropertyName(key) { return key.toLowerCase(); }
 }
-
-class UpperCase extends Json {
-  constructor(data) {
-    super(data);
-  }
-
-  getPropertyName(key) {
-    return key.toUpperCase();
-  }
+class UpperCase extends Json {  
+  constructor(data) { super(data); }
+  getPropertyName(key) { return key.toUpperCase(); }
 }
-
 module.exports.toCamelCase = toCamelCase;
 module.exports.toLowerCase = toLowerCase;
 module.exports.toUpperCase = toUpperCase;
